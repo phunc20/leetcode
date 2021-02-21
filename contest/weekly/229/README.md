eaa82f341310f1a4388fcf007e8eaae85ea78eb7
phunc20 <wucf20.@gmail.com>
2021-02-21 23:08:42 +0700

    try to implement bottom-up DP for Q3 weekly 229

---
+++ contest/weekly/229/README.md
@@ -0,0 +1,7 @@
+## The Day of The Contest
+I only solved the first two questions, browsed thru the description of Q3 and Q4, choosing to focus on Q3 but failed to devise a solution before running out of time.
+
+In the same day, I kind of accused myself of not being able to solve Q3 despite of having spending time pondering upon it.
+So I watched a few videos on dynamic programming, hoping to get some insight; in the afternoon, I seemingly got one idea to solve Q3 using recursion with
+memoization; however, leetcode's website told me that I only passed somewhere btw 29/59 cases, failing because of too long running time. Maybe I should
+convert the solution to bottom-up DP? I wasn't quite sure.
---
+++ contest/weekly/229/max_score/c/memo_recursion.c
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+def maximumScore(nums, multipliers):
+    """
+    Greedy cannot give the best solution.
+    Brute force with care = dynamic programming
+    """
+    m = len(multipliers)
+    n = len(nums)
+    memo = {}
+    def recursive_dp(left, right):
+        """
+        left ~ right (both end included)
+        0 <= left < right <= n-1
+        """
+        if (left, right) in memo:
+            return memo[(left, right)]
+        n_multiplied = n - (right - left + 1)
+        remainig_multipliers = multipliers[n_multiplied:]
+        ## Wrong (when n > m)
+        #if len(remainig_multipliers) == 2:
+        #    maximum = max(nums[left]*remainig_multipliers[0] + nums[right]*remainig_multipliers[1],
+        #                  nums[left]*remainig_multipliers[1] + nums[right]*remainig_multipliers[0])
+        # Correct: 2 is hard, down to 1.
+        if len(remainig_multipliers) == 1:
+            maximum = max(nums[left]*remainig_multipliers[0],
+                          nums[right]*remainig_multipliers[0])
+        else:
+            maximum = max(nums[left]*remainig_multipliers[0] + recursive_dp(left+1, right),
+                          nums[right]*remainig_multipliers[0] + recursive_dp(left, right-1))
+        memo[(left, right)] = maximum
+        #print(f"memo[({left}, {right})] = {maximum}")
+        return maximum
+
+    return recursive_dp(0, n-1)
+
