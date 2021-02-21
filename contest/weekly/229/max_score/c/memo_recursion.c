0e9967a87e47f4b73d7255c64bb8a4c3c2b70bbd
phunc20 <wucf20@gmail.com>
2021-02-22 00:26:35 +0700

    try memoization in C to see if Q3 weekly229 will be accepted, but still buggy

--- contest/weekly/229/max_score/c/memo_recursion.c
+++ contest/weekly/229/max_score/c/memo_recursion.c
@@ -1,7 +1,46 @@
 
 
 
+int maximumScore(int* nums, int numsSize, int* multipliers, int multipliersSize){
+  float memo[numsSize][numsSize];
+  // Initialize the values in memo to empty (We choose 0.5 to represent empty)
+  float empty = 0.5;
+  for (int l=0; l<numsSize; l++) {
+    for (int r=0; r<numsSize; r++) {
+      memo[l][r] = empty;
+    }
+  }
+  float epsilon = 1e-5;
+  int in_memo(int left, int right) {
+    return abs(memo[left][right] - empty) > epsilon;
+  }
+  // (?1) Existe-t-il Dictionary dans C?
+  int recursive_dp(int left, int right)
+  {
+      /* left ~ right (both end included) */
+      /* 0 <= left < right <= n-1         */
+      if (in_memo(left, right))
+        return memo[left][right];
+      int n_multiplied = numsSize - (right - left + 1);
+      int *remainig_multipliers = multipliers + n_multiplied;
+      // Python's if len(remainig_multipliers) == 1 must be translated to the following
+      int ans;
+      int ll, rr;
+      if (multipliersSize - n_multiplied == 1) {
+        ll = *(nums+left)  * (*remainig_multipliers);
+        rr = *(nums+right) * (*remainig_multipliers);
+      }
+      else {
+        ll = *(nums+left)  * (*remainig_multipliers) + recursive_dp(left+1, right);
+        rr = *(nums+right) * (*remainig_multipliers) + recursive_dp(left, right-1);
+      }
+      ans = ll > rr ? ll : rr;
+      memo[left][right] = ans;
+      return ans;
+  }
 
+  return recursive_dp(0, numsSize-1);
+}
 
 
 
