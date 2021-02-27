9eab5f2d44926023ae14ad9c4d65086327a0c01b
phunc20 <wucf20@gmail.com>
2021-02-27 14:37:40 +0700

    more exos

--- problems/database/0176-2nd_highest_salary/schema.sql
+++ problems/database/0176-2nd_highest_salary/schema.sql
@@ -1,5 +1,9 @@
-Create table If Not Exists Employee (Id int, Salary int)
-Truncate table Employee
-insert into Employee (Id, Salary) values ('1', '100')
-insert into Employee (Id, Salary) values ('2', '200')
-insert into Employee (Id, Salary) values ('3', '300')
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+insert into Employee (Id, Salary) values ('4', '300');
+insert into Employee (Id, Salary) values ('5', '400');
+insert into Employee (Id, Salary) values ('6', '500');
+insert into Employee (Id, Salary) values ('7', '600');
--- problems/database/0177-nth_highest_salary/mysql/wrong.sql
+++ problems/database/0177-nth_highest_salary/mysql/wrong.sql
@@ -1,7 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+DECLARE M INT;
+SET M=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      
+      SELECT DISTINCT Salary FROM Employee order by Salary desc offset M limit 1
   );
 END
