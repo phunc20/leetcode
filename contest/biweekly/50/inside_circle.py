06211b8d3886bf47d499e6f496b198f6b9fd81ba
phunc20 <wucf20@gmail.com>
2021-04-17 22:10:53 +0700

    2 first exos

---
+++ contest/biweekly/50/inside_circle.py
@@ -0,0 +1,47 @@
+#class Solution:
+#    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+        
+def countPoints(points, queries):
+    # initialize answer with 0's
+    answer = [0 for _ in range(len(queries))]
+    # brute-force solution
+    def is_inside(pt, circle):
+        x, y ,r = circle
+        u, w = pt
+        return (x-u)**2 + (y-w)**2 <= r**2
+    #for j, (xj, yj, rj) in enumerate(queries):
+    for j, circle_j in enumerate(queries):
+        n_insiders = 0
+        for pt in points:
+            if is_inside(pt, circle_j):
+                n_insiders += 1
+        answer[j] = n_insiders
+    return answer
+
+
+def test(points, queries, expected):
+    sol = countPoints(points, queries)
+    if sol == expected:
+        print("Congratulations!")
+    else:
+        print(f"sol = {sol}")
+        print(f"expected = {expected}")
+    print()
+
+if __name__ == "__main__":
+    test_id = 0
+    
+    test_id += 1
+    print(f"testcase {test_id}")
+    points = [[1,3],[3,3],[5,3],[2,2]]
+    queries = [[2,3,1],[4,3,1],[1,1,2]]
+    expected = [3,2,2]
+    test(points, queries, expected)
+
+    test_id += 1
+    print(f"testcase {test_id}")
+    points = [[1,1],[2,2],[3,3],[4,4],[5,5]]
+    queries = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]]
+    expected = [2,3,2,4]
+    test(points, queries, expected)
+
---
+++ contest/biweekly/50/min_ops.py
@@ -0,0 +1,49 @@
+#class Solution:
+#    def minOperations(self, nums: List[int]) -> int:
+        
+def minOperations(nums):
+    if len(nums) == 1:
+        return 0
+    else:
+        # We make sure each element is strictly greater than its predecessor
+        n_ops = 0
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i-1]
+            if diff <= 0:
+                n_ops += (-diff) + 1
+                nums[i] = nums[i-1] + 1
+    return n_ops
+
+
+def test(nums, expected):
+    sol = minOperations(nums)
+    if sol == expected:
+        print("Congratulations!")
+    else:
+        print(f"sol = {sol}")
+        print(f"expected = {expected}")
+    print()
+
+if __name__ == "__main__":
+    test_id = 0
+    
+    #print(f"testcase {test_id += 1}")
+    test_id += 1
+    print(f"testcase {test_id}")
+    nums = [1,1,1]
+    expected = 3
+    test(nums, expected)
+
+    test_id += 1
+    print(f"testcase {test_id}")
+    nums = [1,5,2,4,1]
+    expected = 14
+    test(nums, expected)
+
+    test_id += 1
+    print(f"testcase {test_id}")
+    nums = [8]
+    expected = 0
+    test(nums, expected)
+
+
